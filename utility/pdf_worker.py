81da00abf4606a233d69b9684b6b8c827eaa7347
theibrahim7 <ibrahim.oduola007@gmail.com>
2021-12-07 13:10:44 +0100

    PyPDF2 isn't so good so I installed PDF Miner.six to help with reading/mining text from the pdfs

--- utility/pdf_worker.py
+++ utility/pdf_worker.py
@@ -1,12 +1,16 @@
-import base64
+#import base64
+#print(base64.a85decode(chunk).decode('utf-8')) #chuck is the chunk of file to be read!
+import PyPDF2
 
 
 def Read_Pdf_File(document):
-	chunck_of_file = document.chunks()
-
-	for chunk in chunck_of_file:
+	with document.open("r") as doc:
+		pdf_reader = PyPDF2.PdfFileReader(doc)
+		reader_on_page = 0 #to start the reader from page 1
+		while reader_on_page < pdf_reader.numPages:
+			print("--------------------------------------")
+			print(pdf_reader.getPage(reader_on_page).extractText())
+			reader_on_page += 1
 		
-		print(base64.a85decode(chunk).decode('utf-8'))
-
 
 	return [["This is a pdf file so I would read this differently"]]
